159f79abc1ce7c75f996c5142d5443aac1c8dc03
Michael Stepniewski <infotechpro.stepniewski@gmail.com>
2014-08-28 14:48:01 -0500

    First Unix Upload
    This is a basic configuration of the Whois program written in perl to
    accept a list of data. However, this upload only is able to handle *nix
    machines that have the whois application installed as there are system
    calls in the program. A to do would be do have a pure perl
    implementation of whois within this script to expand the whois
    functionality and to have the script run on multiple architectures.

---
+++ unix/lswhois.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl -w
+use warnings;
+use strict;
+use diagnostics;
+use FileHandle;
+
+my $inputfile  = FileHandle->new;
+my $outputfile = FileHandle->new;
+my @hosts;
+
+open($inputfile, "<", "input.txt")
+        or die "cannot open < input.txt: $!";
+        @hosts = <$inputfile>;
+close ($inputfile) || warn "close failed: $!";
+
+qx(touch output.txt);
+
+open($outputfile, ">>", "output.txt")
+        or die "cannot open > output.txt: $!";
+
+foreach my $hosts (@hosts) {
+		print $outputfile $hosts."\n";
+        $hosts = qx(whois $hosts);
+        print $outputfile $hosts."\n";
+}
+
+close ($outputfile) || warn "close failed: $!";
+
+my @lines;
+
+open($outputfile, "<", "output.txt")
+        or die "cannot open < output.txt: $!";
+
+my $regex = "Name Server: ";
+
+@lines = grep { /\Q$regex/ } <$outputfile>;
+
+close ($outputfile) || warn "close failed: $!";
+
+for my $lines (@lines) {
+        print $lines."\n";
+}
