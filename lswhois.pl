138795613a94ba2c4ab243c2c786603ba69afde9
Michael Stepniewski <infotechpro.stepniewski@gmail.com>
2014-09-08 14:40:07 -0500

    Create lswhois.pl

---
+++ lswhois.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl -w
+use warnings;
+use strict;
+use diagnostics;
+use FileHandle;
+
+my $inputfile  = FileHandle->new;
+my $outputfile = FileHandle->new;
+my @hosts;
+
+open($inputfile, "<", "input.txt")
+        or die "cannot open < input.txt: $!";
+        @hosts = <$inputfile>;
+close ($inputfile) || warn "close failed: $!";
+
+qx(touch output.txt);
+
+open($outputfile, ">>", "output.txt")
+        or die "cannot open > output.txt: $!";
+
+foreach my $hosts (@hosts) {
+	print $outputfile $hosts."\n";
+        $hosts = qx(whois $hosts);
+        print $outputfile $hosts."\n";
+}
+
+close ($outputfile) || warn "close failed: $!";
